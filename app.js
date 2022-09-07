d1eaa81c68495484fe34e5907d105454c4b05d9e
aaronlrv <aaronvitharanage@gmail.com>
2022-09-07 10:51:34 +0400

    Allow about-me url to load about me page

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-var fs = require("fs");
-var http = require("http");
+let fs = require("fs");
+let http = require("http");
 
 http
   .createServer(function (request, response) {
+    console.log(request.url);
     response.writeHead(200, { "Content-Type": "text/html" });
 
-    var file = fs.createReadStream("index.html");
-    file.pipe(response);
+    if (request.url === "/") {
+      var file = fs.createReadStream("index.html");
+      file.pipe(response);
+    }
+
+    if (request.url === "/about-me") {
+      var file = fs.createReadStream("about-me.html");
+      file.pipe(response);
+    }
   })
   .listen(8080);
 
 console.log("listening on port 8080...");
+
 // server.listen(port, () => {
 //   console.log(`Server running at port ${port}`);
 // });
